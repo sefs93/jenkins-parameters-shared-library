dbcd0a88b2b931ae3ffcb0a5c8a310c1704ab67b
Stanislav Polchanikov <sefs93@gmail.com>
2021-01-23 09:20:31 +0200

    remove try-catch block

--- vars/reloadParameters.groovy
+++ vars/reloadParameters.groovy
@@ -1,15 +1,10 @@
 //opts.reload_parameters
 def call () {
-    try {
-        if (params.RELOAD_PARAMETERS1) {
-            stage('Reload parameters') {
-                currentBuild.displayName = 'RELOAD_PARAMETERS'
-                currentBuild.result = 'ABORTED'
-                error('RELOAD_PARAMETERS')
-            }
+    if (params.RELOAD_PARAMETERS1) {
+        stage('Reload parameters') {
+            currentBuild.displayName = 'RELOAD_PARAMETERS'
+            currentBuild.result = 'ABORTED'
+            error('RELOAD_PARAMETERS')
         }
-    } catch (e) {
-        println e
-        println "[WARN] RELOAD_PARAMETERS parameter is not defined yet. Skipping"
     }
 }

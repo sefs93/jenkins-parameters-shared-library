1d227c82c22427566cea04c5ae66d89aa79e5b57
Stanislav Polchanikov <sefs93@gmail.com>
2021-01-23 09:18:41 +0200

    chack exception class if param if not defined

--- vars/reloadParameters.groovy
+++ vars/reloadParameters.groovy
@@ -1,7 +1,7 @@
 //opts.reload_parameters
 def call () {
     try {
-        if (params.RELOAD_PARAMETERS) {
+        if (params.RELOAD_PARAMETERS1) {
             stage('Reload parameters') {
                 currentBuild.displayName = 'RELOAD_PARAMETERS'
                 currentBuild.result = 'ABORTED'

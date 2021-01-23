f65d47588a03fc0e8e5d79a408da00ac896c7d52
Stanislav Polchanikov <sefs93@gmail.com>
2021-01-23 11:25:17 +0200

    Add dynamic parameters for service choice

--- vars/params_SERVICE_VERSION.groovy
+++ vars/params_SERVICE_VERSION.groovy
@@ -1,3 +1,30 @@
 def call() {
-    return string(name: 'SERVICE_VERSION')
+    return [$class: 'CascadeChoiceParameter',
+        choiceType: 'PT_SINGLE_SELECT',
+        description: '',
+        filterLength: 3,
+        filterable: true,
+        name: 'SERVICE_VERSION',
+        randomName: 'choice-parameter-1083368143661600',
+        referencedParameters: 'SERVICE_NAME',
+        script: [$class: 'GroovyScript',
+            fallbackScript:[
+                classpath: [],
+                sandbox: false,
+                script: ''
+            ],
+            script: [
+                classpath: [],
+                sandbox: false,
+                script: '''def command = 'ls -1t /var/lib/jenkins/userContent/services/\${SERVICE_NAME}/'
+                def proc = command.execute()
+                proc.waitFor()
+
+                def output = proc.in.text
+
+                return output.tokenize()
+                '''
+            ]
+        ]
+    ]
 }
